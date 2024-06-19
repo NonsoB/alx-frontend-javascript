03c657ed555f61d43ef01b8c59c58d94e8347fe9
“NonsoB” <okorojichinonso1@gmail.com>
2024-06-19 10:23:58 -0700

    task_0

--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -1,48 +1,36 @@
-interface Student {
-    firstName: string;
-    lastName: string;
-    age: number;
-    location: string;
+/*interface Teacher {
+    firstName: string,
+    lastName: string,
+    location: string,
+    fullTimeEmployee: true | false,
+    yearsOfExperience?: number,
+    contract?: any
+}
+interface Directors extends Teacher {
+    numberOfReports: number
+}*/
+
+interface user {
+    firstName: string,
+    lastName: string
 }
 
-const student1: Student = {
-    firstName: 'firdaus',
-    lastName: 'salim',
-    age: 21,
-    location: 'mombasa',
-};
-const student2: Student = {
-    firstName: 'beta',
-    lastName: 'scribbles',
-    age: 25,
-    location: 'kenya',
-};
+class StudentClass {
+    firstName: string;
+    lastName: string;
 
-const studentsList: Student[] = [student1, student2];
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName,
+        this.lastName = lastName
+    }
 
-const table = document.createElement('table');
-const tbody = document.createElement('tbody');
+    workOnHomework() {
+        console.log("Currently working")
+        return ("Currently working")
+    }
 
-table.style.background = "lightgray";
-table.appendChild(tbody);
-
-studentsList.forEach((student: Student): void => {
-    const row = document.createElement('tr');
-
-    const nameCell = document.createElement('td');
-    const locationCell = document.createElement('td');
-
-    nameCell.textContent = student.firstName;
-    locationCell.textContent = student.location;
-
-    nameCell.style.border = "2px solid white";
-    locationCell.style.border = "2px solid white";
-    nameCell.style.padding = "4px";
-    locationCell.style.padding = "4px";
-
-    row.appendChild(nameCell);
-    row.appendChild(locationCell);
-    tbody.appendChild(row);
-});
-
-document.body.appendChild(table);
+    displayName() {
+        console.log(this.firstName)
+        return this.firstName;
+    }
+}
